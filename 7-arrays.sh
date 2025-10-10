da9e597f75979c07fbd936606807a03d12144ec7
anithaaluru <info@devops.com>
2025-10-10 12:46:21 +0530

    helloworld started

--- 7-arrays.sh
+++ 7-arrays.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 Movies=("hi!Nanna" "8vasanthsalu" "pushpa2" "mad")
 echo "first movie is : ${Movies[0]}"
-echo "first movie is : ${Movies[3]}"
-echo "first movie is : ${Movies[@]}"
+echo "second movie is : ${Movies[3]}"
+echo "all movies are : ${Movies[@]}"
