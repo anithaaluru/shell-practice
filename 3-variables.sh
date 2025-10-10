29d2a3389f5c83c934a3e31b7f4f3def5b747371
anithaaluru <info@devops.com>
2025-10-10 09:18:52 +0530

    helloworld started

---
+++ 3-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+Person1=Jon
+Person2=Don
+echo "$Person1:hey!$Person2,how are you?"
+echo "$Person2:hi!$Person1,iam good.what about you?"
+echo "$Person1:Iam too good.Glad to see you after so many days."
+echo "$Person2:yeah!where are you working now?"
+echo "$Person1:Iam working in a coffee shop as a waiter.what about you?"
+echo "$Person2:Iam working as a manager in a saloon shop."
+echo "$Person1:Ho nice man!whenever you will be free visit our coffee shop."
+echo "$Person2:yeah sure!catch you later,bye!"
+echo "$Person1:Bye bye!"
