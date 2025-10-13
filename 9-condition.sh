9a61c38a105e8e9165e4af3df8ce164729db97b2
anithaaluru <info@devops.com>
2025-10-13 20:18:59 +0530

    special variables

--- 9-condition.sh
+++ 9-condition.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 Number=$1
-if [ $Number -lt 10 ] {
-    then
+if [ $Number -lt 10 ] 
+then
     echo "$Number is less than 10"
-}
 else
-{
     echo "$Number is not less than 10"
-}
 fi
