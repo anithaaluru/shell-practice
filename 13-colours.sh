48fca80ff92f87de833b2f0ed66468fc95c00897
anithaaluru <info@devops.com>
2025-10-18 13:06:02 +0530

    colors

--- 13-colours.sh
+++ 13-colours.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 USERID=$(id -u)
-R="/e[31m"
-G="/e[32m"
-Y="/e[33m"
-N="/e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 if [ $USERID -ne 0 ]
 then
  echo -e "$R ERROR::please run this script with root access $N"
