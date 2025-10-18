9d9116859e30abe1d52098051e0ca80f430a40eb
anithaaluru <info@devops.com>
2025-10-18 12:56:35 +0530

    colors

--- 13-colours.sh
+++ 13-colours.sh
@@ -9,44 +9,44 @@ then
  echo -e "$R ERROR::please run this script with root access $N"
  exit 1
 else
- echo " $Y you are running this script with root access $N"
+ echo -e "$Y you are running this script with root access $N"
  fi
 
 VALIDATE () 
  if [ $1 -ne 0 ]
   then
-   echo "$2 installation is $R failure $N"
+   echo -e "$2 installation is $R failure $N"
  else
-   echo "$2 installation is  $G success $N"
+   echo -e "$2 installation is  $G success $N"
    exit 1
  fi
 
 dnf list installed mysql
  if [ $? -ne 0 ]
  then 
-  echo "$Y mysql is not installed...going to install it $N"
+  echo -e "$Y mysql is not installed...going to install it $N"
   dnf install mysql -y
   VALIDATE $? "mysql"
  else
-  echo "$G mysql is already installed..Nothing to do $N"
+  echo -e "$G mysql is already installed..Nothing to do $N"
  fi 
 
  dnf list installed python3
  if [ $? -ne 0 ]
  then 
-  echo "$Y python3 is not installed...going to install it $N"
+  echo -e "$Y python3 is not installed...going to install it $N"
   dnf install python3 -y
   VALIDATE $? "python3"
  else
-  echo "$G python3 is already installed..Nothing to do $N"
+  echo -e "$G python3 is already installed..Nothing to do $N"
  fi 
 
  dnf list installed nginx
  if [ $? -ne 0 ]
  then 
-  echo "$Y nginx is not installed...going to install it $N"
+  echo -e "$Y nginx is not installed...going to install it $N"
   dnf install nginx -y
   VALIDATE $? "nginx"
  else
-  echo "$G nginx is already installed..Nothing to do $N"
+  echo -e "$G nginx is already installed..Nothing to do $N"
  fi 
