6606807fabc97d0276641bbcd1c279a281090c4a
anithaaluru <info@devops.com>
2025-10-18 13:44:42 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -31,7 +31,6 @@ dnf list installed mysql &>>$LOG_FILE
  if [ $? -ne 0 ]
  then 
   echo -e "$Y mysql is not installed...going to install it $N" | tee -a $LOG_FILE
- fi 
   dnf install mysql -y &>>$LOG_FILE
   VALIDATE $? "mysql"
  else
@@ -42,7 +41,6 @@ dnf list installed mysql &>>$LOG_FILE
  if [ $? -ne 0 ]
  then 
   echo -e "$Y python3 is not installed...going to install it $N" | tee -a $LOG_FILE
- fi 
   dnf install python3 -y &>>$LOG_FILE
   VALIDATE $? "python3"
  else
@@ -53,7 +51,6 @@ dnf list installed mysql &>>$LOG_FILE
  if [ $? -ne 0 ]
  then 
   echo -e "$Y nginx is not installed...going to install it $N" | tee -a $LOG_FILE
- fi 
  dnf install nginx -y  &>>$LOG_FILE
   VALIDATE $? "nginx"
  else
