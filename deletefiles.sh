81caa091ad30905d668145cefa6a5fe89ab1c973
anithaaluru <info@devops.com>
2025-11-05 19:26:16 +0530

    helloworld

--- deletefiles.sh
+++ deletefiles.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
 USERID=$(id -u)
 SOURCE_DIR=$1
 DEST_DIR=$2
-Days=${3:-14}
-LOGS_FOLDER="/var/log/delete-log"
+DAYS=${3:-14} # if DAYS are provided that will be considered, otherwise default 14 days
+
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 R="\e[31m"
@@ -11,41 +13,55 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R ERROR:: Please run this script with root access $N"
+        exit 1 #give other than 0 upto 127
+    else
+        echo "You are running with root access" 
+    fi
+
 mkdir -p $LOGS_FOLDER
 
-if [ $USERID -ne 0 ]
-then
- echo -e "$R ERROR::please run this script with root access $N"
- exit 1
-else
- echo -e "$Y you are running this script with root access $N"
- fi
- USAGE(){
-      echo "USAGE::sh deletefiles.sh <source-dir> <dest-dir> <days>"
- }
+USAGE(){
+    echo -e "$R USAGE:: $N sh deletefiles.sh <source-dir> <destination-dir> <days(optional)>"
+    exit 1
+}
 
- if [ $# -lt 3 ]
- then 
-   USAGE
+if [ $# -lt 2 ]
+then
+    USAGE
 fi
 
 if [ ! -d $SOURCE_DIR ]
- then
-  echo -e "$Y source directory $SOURCE_DIR does not exist.please check $N"
-  exit 1
+then
+    echo -e "$R Source Directory $SOURCE_DIR does not exist. Please check $N"
+    exit 1
 fi
 
 if [ ! -d $DEST_DIR ]
- then
-  echo -e "$Y destination directory $DEST_DIR does not exist.please check $N"
-  exit 1
+then
+    echo -e "$R Destination Directory $DEST_DIR does not exist. Please check $N"
+    exit 1
 fi
 
-Files=$(find $SOURCE_DIR -name "*.log" -mtime +$Days)
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ ! -z $Files ]
-then 
-echo "log files are found"
+if [ ! -z "$FILES" ]
+then
+    echo "files are not found"
+    
 else
- echo "log files are not exist"
+    echo "files are found"
 fi
