60deed846ebbc150fb571f2903bd07dc33eddf41
anithaaluru <info@devops.com>
2025-11-10 20:48:07 +0530

    helloworld

---
+++ disk_usage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+disk_usage=$(df -hT | grep -v Filesystem)
+disk_threshold=1
+while IFS= read -s $line
+do
+   usage=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
+   partition=$(echo $line | awk '{print $7F}')
+   echo "$partition:$usage"
+
+
+done <<< $disk_usage
