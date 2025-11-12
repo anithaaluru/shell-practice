bf8875d3b6823a5c4460368797576cfd9b9db9b3
anithaaluru <info@devops.com>
2025-11-12 11:25:06 +0530

    helloworld

--- diskusage.sh
+++ diskusage.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 disk_usage=$(df -hT | grep -v Filesystem)
 disk_threshold=1
+MSG=" "
 while IFS= read line
 do
    usage=$(echo $line | awk '{print $6F}' | cut -d "%" -f1)
    partition=$(echo $line | awk '{print $7F}')
    if [ $usage -ge $disk_threshold ]
      then
-       echo "there is high disk usage $partition: $usage"
+       Msg=There is high disk usage $partition: $usage
    fi
 done <<< $disk_usage
+echo "MSG"
