c707eb165f22cbc1a4287f55ab651951d542f16e
anithaaluru <info@devops.com>
2025-11-12 13:28:38 +0530

    helloworld

--- mail.sh
+++ mail.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-To_Team=$1
+
+TO_TEAM=$1
 ALERT_TYPE=$2
-IP_Address=$3
+IP_ADDRESS=$3
 MSG=$4
-FINAL_MSG=$(printf '%s\n' "MSG" | sed -e 's/[] /$*/\./,#/g')
+FINAL_MSG=$(printf '%s\n' "$MSG" | sed -e 's/[]\/$*.^[]/\\&/g')
 TO_ADDRESS=$5
 SUBJECT=$6
 
-FINAL_BODY=$(sed -e "s|To_TEAM|$To_TEAM|g" -e "s|ALERT_TYPE|$ALERT_TYPE|g" -e "s|IP_Address|$IP_Address|g" -e "s|MSG|$FINAL_MSG|g" -e "s|TO_ADDRESS|$TO_ADDRESS|g" -e "s|SUBJECT|$SUBJECT|g" Template.html)
+FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MSG/$FINAL_MSG/g" Template.html) 
 
 {
 echo "To: $TO_ADDRESS"
